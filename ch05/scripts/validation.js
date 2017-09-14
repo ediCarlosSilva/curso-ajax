322cd023c12c6256d85fc0ccf800cfbec44a7f86
Edi Carlos note <edi_csilva@hotmail.com>
2017-09-14 11:44:37 -0300

    conclusao c5

--- ch05/scripts/validation.js
+++ ch05/scripts/validation.js
@@ -1,9 +1,12 @@
 window.onload = initPage;
+var usernameValid = false;
+var passwordValid = false;
 
 function initPage() {
     document.getElementById("username").onblur = checkUsername;
     document.getElementById("password2").onblur = checkPassword;
     document.getElementById("register").disabled = true;
+    document.getElementById("register").onclick = registerUser;
 }
 
 function checkPassword() {
@@ -14,6 +17,8 @@ function checkPassword() {
     // primeiro comparando as senhas
     if ((password1.value == "") || (password1.value != password2.value)) {
         password1.className = "denied";
+        passwordValid = false;
+        checkFormStatus();
         return;
     }
 
@@ -36,13 +41,16 @@ function showPasswordStatus() {
             var password1 = document.getElementById("password1");
             if (passwordRequest.responseText == "okay") {
                 password1.className = "approved";
-                document.getElementById("register").disabled = false;
+                //document.getElementById("register").disabled = false;
+                passwordValid = true;
             } else {
                 password1.className = "denied";
                 password1.focus();
                 password1.select();
-                document.getElementById("register").disabled = true;
+                //document.getElementById("register").disabled = true;
+                passwordValid = false;
             }
+            checkFormStatus();
         }
     }
 }
@@ -67,13 +75,66 @@ function showUsernameStatus() {
         if (usernameRequest.status == 200) {
             if (usernameRequest.responseText == "okay") {
                 document.getElementById("username").className = "approved";
-                document.getElementById("register").disabled = false;
+                //document.getElementById("register").disabled = false;
+                usernameValid = true;
             } else {
                 document.getElementById("username").className = "denied";
                 document.getElementById("username").focus();
                 document.getElementById("username").select();
-                document.getElementById("register").disabled = true;
+                //document.getElementById("register").disabled = true;
+                usernameValid = false;
             }
+            checkFormStatus();
+        }
+    }
+}
+
+function checkFormStatus() {
+    if (usernameValid && passwordValid) {
+        document.getElementById("register").disabled = false;
+    } else {
+        document.getElementById("register").disabled = true;
+    }
+}
+
+function registerUser() {
+    t = setInterval("scrollImages()", 50);
+    document.getElementById("register").value = "Processando...";
+    registerRequest = createRequest();
+    if (registerRequest == null) {
+        alert("Incapaz de criar a solicitação.");
+    } else {
+        var url = "register.php?username=" +
+            escape(document.getElementById("username").value) + "&password=" +
+            escape(document.getElementById("password1").value) + "&firstname=" +
+            escape(document.getElementById("firstname").value) + "&lastname=" +
+            escape(document.getElementById("lastname").value) + "&email=" +
+            escape(document.getElementById("email").value) + "&genre=" +
+            escape(document.getElementById("genre").value) + "&favorite=" +
+            escape(document.getElementById("favorite").value) + "&tastes=" +
+            escape(document.getElementById("tastes").value);
+        registerRequest.onreadystatechange = registrationProcessed;
+        registerRequest.open("GET", url, true);
+        registerRequest.send(null);
+    }
+}
+
+function scrollImages() {
+    var coverBarDiv = document.getElementById("coverBar");
+    var images = coverBarDiv.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+        var left = images[i].style.left.substr(0, images[i].style.left.length - 2);
+        if (left <= -86) {
+            left = 532;
+        }
+        images[i].style.left = (left - 1) + "px";
+    }
+}
+
+function registrationProcessed() {
+    if (registerRequest.readyState == 4) {
+        if (registerRequest.status == 200) {
+            document.getElementById("wrapper").innerHTML = registerRequest.responseText;
         }
     }
 }
